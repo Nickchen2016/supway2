83b34aaf5aacb88367ec107f606f797381231f8a
Nick Chen <chenyahua2012@hotmail.com>
2020-03-06 23:43:55 -0500

    front page working environemnt setup

--- App.vue
+++ App.vue
@@ -1,28 +1,51 @@
-  
 <template>
-  <app-navigator></app-navigator>
+  <view class="container">
+        <status-bar hidden/>
+    <view :class="isActive">
+      <text class="text-color-primary">{{message}}</text>
+    </view>
+    <view class="sub-views">
+        <text class="text-color-primary">{{dates}}</text>
+    </view>
+    <view class="sub-views">
+        <text class="text-color-primary">{{years}}</text>
+
+    </view>
+  </view>
 </template>
-
 <script>
-import {
-  createAppContainer,
-  createStackNavigator
-} from "vue-native-router";
-import SelectScreen from "./client/SelectScreen.vue";
-import DetailsScreen from "./client/DetailsScreen.vue";
-const StackNavigator = createStackNavigator(
-  {
-    Home: SelectScreen,
-    Details: DetailsScreen
-  },
-  {
-    initialRouteName: 'Home'
-  }
-);
-const AppNavigator = createAppContainer(StackNavigator);
 export default {
-  components: { AppNavigator }
-}
+  data: function() {
+    return {
+      message: "Hello World",
+      dates: 'Hello you!',
+      years: 'hyfuy',
+      isActive: 'isAction'
+    };
+  }
+};
 </script>
-
-
+<style>
+.container {
+  flex: 1;
+  align-items: center;
+  /* justify-content: center; */
+  background-color: antiquewhite;
+  /* background-color: rgba(0,0,51,1); */
+}
+.sub-views {
+  width: 100%;
+  height: 38%;
+  background-color: green;
+  /* flex: 1; */
+}
+.text-color-primary {
+  color: blue;
+  font-size: 30;
+}
+.isAction {
+  width: 100%;
+  height: 24%;
+  background-color: red;
+}
+</style>
